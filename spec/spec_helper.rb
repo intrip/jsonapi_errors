cb0194019a885a904c562a383b8784fe22384b16
Jacopo <beschi.jacopo@gmail.com>
2016-03-12 17:20:27 +0100

    fixing case sensitive load files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'JSONAPI_errors'
+require 'jsonapi_errors'
 require 'byebug'
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
 
