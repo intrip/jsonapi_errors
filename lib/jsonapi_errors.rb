c4b40d19beef91632784beb6b1b4056d1cb21ff6
Jacopo <beschi.jacopo@gmail.com>
2016-03-12 17:28:37 +0100

    fixed more case sensitive errors

--- lib/jsonapi_errors.rb
+++ lib/jsonapi_errors.rb
@@ -2,10 +2,10 @@ require "active_support"
 require "active_support/concern"
 require "active_support/core_ext/hash"
 require "active_support/core_ext/class/attribute"
-require "JSONAPI_errors/version"
-require "JSONAPI_errors/configuration"
-require "JSONAPI_errors/hash_renderer"
-require "JSONAPI_errors/rails/controller"
+require "jsonapi_errors/version"
+require "jsonapi_errors/configuration"
+require "jsonapi_errors/hash_renderer"
+require "jsonapi_errors/rails/controller"
 
 module JSONAPIErrors
   module Errors
